bb06cb927a8c66dda12c6275eb70f6e141bca80d
Pierre Santos <santospierre1305@gmail.com>
2021-01-31 17:59:18 +0100

    v0.2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
-// import reportWebVitals from './reportWebVitals';
 
 import './index.css';
 import App from './App';
 
-import { store /*persistor*/ } from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            {/* <PersistGate persistor={persistor}> */}
             <App />
-            {/* </PersistGate> */}
         </Provider>
     </React.StrictMode>,
     document.getElementById('root')
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,10 @@
 import mainReducer from './mainReducer';
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-// import { persistStore, persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
+
 import thunk from 'redux-thunk';
 
-// const persistConfig = {
-//     key: 'react_spa',
-//     storage,
-// };
-
-// const persisterReducer = persistReducer(persistConfig, mainReducer);
-
 export const store = createStore(
-    /* If want to use persist replace by persisterReducer */
     mainReducer,
     composeWithDevTools(applyMiddleware(thunk))
 );
-// export const persistor = persistStore(store);
