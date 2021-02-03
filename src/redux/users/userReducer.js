07d7abbafc8a19a3db35db1941182ba8a85bc247
Pierre Santos <santospierre1305@gmail.com>
2021-02-03 13:57:41 +0100

    v1.1.2

--- src/redux/users/userReducer.js
+++ src/redux/users/userReducer.js
@@ -5,7 +5,7 @@ const INITIAL_STATE = null;
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case userTypes.SET_USER:
-            return { ...action.payload };
+            return { ...state, ...action.payload };
         default:
             return state;
     }
